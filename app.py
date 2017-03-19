efd58fd9effd229adec6c9ed08c09bba7cb2d188
Appu A S <appu.as@livestream.com>
2017-03-19 17:39:39 +0530

    Basic forms and actions

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, url_for
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+@app.route('/')
+def form():
+    return render_template('form_submit.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/manage/', methods=['POST'])
+def manage_user():
+    manage_action=request.form['manageAction']
+    if manage_action == 'add':
+        return "adding user"
+    #return render_template('form_action.html', name=name, email=email)
+
+# Run the app :)
+if __name__ == '__main__':
+  app.run()
