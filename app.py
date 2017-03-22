a00491517dd57e555c9c8b24a513b38a7d67b2a9
Appu A S <appu.as@livestream.com>
2017-03-22 09:45:33 +0530

    included a class for datatype and working usermodification

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 import crypt
 from jinja2 import Template
 
+
+
 class UserData():
     def __init__(self, userName, fullName, homeDir, shellType, userPassword):
         # you can put here some validation logic
@@ -14,14 +16,16 @@ class UserData():
         self.userSudo = userSudo
         self.userPassword = userPassword
 
+
 app = Flask(__name__)
 
 
 def createUser(userdata):
 
     encPass = crypt.crypt(userdata.userPassword,"22")   
-    return  os.system("useradd -p "+encPass+ " -s "+ userdata.shellType + " -d "+ userdata.homeDir + " -m " + " -c \"" + userdata.fullName + "\" " + userdata.userName)
-
+    if userdata.userSudo == "True":
+        return  os.system("useradd -p "+encPass+ " -s "+ userdata.shellType + " -d "+ userdata.homeDir + " -m " + " -c \"" + userdata.fullName + "\" " + userdata.userName + "&& usermod -aG sudo "+ userdata.userName )
+    return  os.system("useradd -p "+encPass+ " -s "+ userdata.shellType + " -d "+ userdata.homeDir + " -m " + " -c \"" + userdata.fullName + "\" " + userdata.userName )
 
 def listUser():
     user_list = os.popen('awk -F: \'$2 != "*" && $2 !~ /^!/ { print $1}\' /etc/shadow').read()
@@ -48,6 +52,23 @@ def getUserdata(username):
         return False
 
 
+def updateUserdata(userdata):
+    status = 0
+    print old_user_name
+    print old_user_sudo
+    status += os.system("usermod -c \"%s\" %s" %(userdata.fullName, old_user_name)) #updating full name
+    status += os.system("usermod -d %s %s" %(userdata.homeDir, old_user_name)) #updating home dir
+    status += os.system("usermod -s %s %s" %(userdata.shellType, old_user_name)) #updating shell
+    #updating sudo
+    if userdata.userSudo == "True" and old_user_sudo == "False":
+        status += os.system("usermod -aG sudo %s" %old_user_name)
+    elif userdata.userSudo == "False" and old_user_sudo == "True":
+        status += os.system("deluser %s sudo" %old_user_name)
+    #updating username
+    status += os.system("usermod -l %s %s" %(userdata.userName, old_user_name))
+    if status == 0:
+        return True
+    return status
 
 @app.route('/')
 def form():
@@ -83,11 +104,27 @@ def add_user():
 def modify_user():
 
     user_data = getUserdata(request.form['userName'])
+    global old_user_name 
+    old_user_name = user_data.userName
+    global old_user_sudo
+    old_user_sudo = user_data.userSudo
     if user_data:
         return render_template('form_modify.html',userdata = user_data)
     else:
-        return 'This user does nto exist.'
+        return 'This user does not exist.'
 
+@app.route('/updateuser/', methods=['POST'])
+def update_user():
+
+    UserData.userName = request.form['userName']
+    UserData.fullName = request.form['fullName']
+    UserData.homeDir = request.form['homeDir']
+    UserData.shellType = request.form['shellType']
+    UserData.userPassword = request.form['userPassword']
+    UserData.userSudo = request.form['userSudo']
+    if updateUserdata(UserData):
+        return "User detailes updates successfully"
+    return "User details updation failed"
 
 @app.route('/removeuser/', methods=['POST'])
 def remove_user():
